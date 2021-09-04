ab57f91afb83946584ee705feb2687cf936f5638
ElmerAdrianV <elmeradrianv@outlook.com>
2021-09-03 22:55:13 -0500

    Arreglando insertion_sort

--- InsertionSort.py
+++ InsertionSort.py
@@ -4,13 +4,16 @@ Created on Fri Sep 03 20:25:09 2021
 
 @author: ElmerAdrianV
 """
+import random
+from datetime import datetime # Fuente: https://docs.python.org/es/3/library/datetime.html
 
-def InsertionSort(input, length):
-    for i in range(1,length-1):
+def insertion_sort(input, length):
+    for i in range(1,length,1):
         a=input[i]
         j=i
-        while(0<j & a<input[j-1]):
+        while(j>0 and a<input[j-1]):
             input[j]=input[j-1]
-            j-=1
-        input[j]=a
+            j=j-1
+        input[j]=a 
+    return input;
 
